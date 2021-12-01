3f6db22180a418a71e480d92ca788cf50d73d9a2
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-12-01 09:24:47 -0800

    Update rock-output.js

--- rock-output.js
+++ rock-output.js
@@ -1,7 +1,7 @@
 const imageArray = [
 	"https://raw.githubusercontent.com/skinnybeans17/three-dice-rolls-outcomes/main/Rock%20Breaks%20Scissors.png",
-    "https://static.wikia.nocookie.net/battlefordreamisland/images/b/b0/Paper_Covers_Rock.png/revision/latest?cb=20211123181933",
-	"https://static.wikia.nocookie.net/battlefordreamisland/images/6/6e/Rocks_Tie.png/revision/latest?cb=20211124164835"
+    "https://raw.githubusercontent.com/skinnybeans17/three-dice-rolls-outcomes/main/Paper%20Covers%20Rock.png",
+	"https://raw.githubusercontent.com/skinnybeans17/three-dice-rolls-outcomes/main/Rocks%20Tie.png"
 ];
 
 const image = document.querySelector("img");
